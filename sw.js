48994893629b550ed960266fe6a4bd2c1bba0b68
metabolic240 <metabolicdisease@yahoo.com>
2025-08-08 08:22:40 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,38 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "tio-pepes-game-night-v3"; // bump version each time you redeploy
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/styles.css",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('tio-pepes-v1').then(cache => cache.addAll([
-      './',
-      './index.html',
-      './style.css',
-      './app.js',
-      './manifest.json',
-      './icons/icon-192.png',
-      './icons/icon-512.png',
-      './icons/icon-180.png'
-    ])).catch(()=>{})
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
   );
 });
